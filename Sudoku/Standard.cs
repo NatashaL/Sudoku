e45cfbeab4cf48dc5494e117ae721f101dfb846a
Natasha Lazarova <lazarovanatasha@gmail.com>
2013-05-19 23:20:10 +0200

    New Classes

---
+++ Sudoku/Standard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class Standard : Sudoku
+    {
+        public PuzzleGrid puzzleGrid;
+        public PuzzleSolver puzzleSolver;
+        public PuzzleGenerator puzzleGenerator;
+
+        public Standard(Difficulty diff): base (diff)
+        {
+            puzzleGrid = new PuzzleGrid();
+            puzzleSolver = new PuzzleSolver();
+            puzzleGenerator = new PuzzleGenerator(diff);
+
+            puzzleGenerator.InitGrid();
+            base.solution = puzzleGenerator.SolutionGrid.Grid;
+            base.mask = puzzleGenerator.PermaGrid.Grid;//check for negative values 
+
+
+        }
+
+    }
+}
---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class Sudoku
+    {
+        public int[,] mask;
+        public int[,] solution;
+        public int[,] scheme;
+        public Difficulty diff;
+
+        public Sudoku(Difficulty diff)
+        {
+            mask = new int[9, 9];
+            solution = new int[9, 9];
+            scheme = new int[9, 9];
+            this.diff = diff;
+        }
+        public static bool isSolved(int[,] grid)
+        {
+            return true;
+        }
+
+    }
+}
